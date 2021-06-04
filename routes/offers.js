c6eb876a9669b733e94438cfec7a7aab4c1f9f42
Lhazael <lisa.hazael@gmail.com>
2021-06-04 14:58:00 +0200

    offers routes done

--- routes/offers.js
+++ routes/offers.js
@@ -1,30 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Offer = require('../models/Offer');
+const Offer = require("../models/Offer");
 
 // api/offers
 // GET ALL OFFERS IN THE DB
 
 router.get("/", (req, res) => {
-    Offer.find()
+  Offer.find()
     .then((documentOffers) => {
-        res.status(200).json(documentOffers)
+      res.status(200).json(documentOffers);
     })
-    .catch ((err) => {
-        console.log(err)
-        res.status(500).json({message : 'Cannot find any offer'})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Cannot find any offer" });
     });
 });
 
 // GET ONE OFFER IN THE DB
 
 router.get("/:id", (req, res) => {
-    Offer.findById(req.params.id)  // (req.body) => input  // (req.params.id) => url 
+  Offer.findById(req.params.id) // (req.body) => input  // (req.params.id) => url
     .then((documentOffer) => {
-        res.status(200).json(documentOffer)
+      res.status(200).json(documentOffer);
     })
-    .catch ((err) => {
-        res.status(500).json({message: 'Cannot find any offer'})
+    .catch((err) => {
+      res.status(500).json({ message: "Cannot find any offer" });
+    });
+});
+
+// CREATE AN OFFER IN THE DB
+
+router.post("/", (req, res) => {
+  const { title, description, condition, size, lookingFor, picture, price } =
+    req.body;
+
+  const newOffer = {
+    title,
+    description,
+    condition,
+    size,
+    lookingFor,
+    picture,
+    price,
+  };
+
+  Offer.create(newOffer)
+    .then((documentOffer) => {
+      res.status(200).json(documentOffer);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Cannot create offer" });
+    });
+});
+
+// UPDATE AN OFFER
+
+router.patch("/:id", (req, res) => {
+  Offer.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((documentOffer) => {
+      res.status(200).json(documentOffer);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Cannot update offer" });
+    });
+});
+
+// DELETE AN OFFER
+
+router.delete("/:id", (req, res) => {
+  Offer.findByIdAndRemove(req.params.id)
+    .then((documentOffer) => {
+      res.status(200).json(documentOffer, { message: "Deleted" })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Cannot delete offer" })
     });
 });
 
